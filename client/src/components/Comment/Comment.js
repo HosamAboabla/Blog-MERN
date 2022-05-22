512c19ce35790bf7328de14149ac0a8a70ffe812
HosamAboabla <hosamaboabla@yahoo.com>
2022-05-22 18:20:50 +0200

    Adding post comments

--- client/src/components/Comment/Comment.js
+++ client/src/components/Comment/Comment.js
@@ -1,6 +1,6 @@
 
 
-const Comment = () => {
+const Comment = ({ comment}) => {
 
 
     return (
@@ -9,11 +9,11 @@ const Comment = () => {
                 <div className="user d-flex align-items-center">
                 <div className="image">
                     <img src="img/user.svg" alt="..." className="img-fluid rounded-circle" /></div>
-                <div className="title"><strong>Jabi Hernandiz</strong><span className="date">May 2016</span></div>
+                <div className="title"><strong>{comment.user}</strong><span className="date">May 2016</span></div>
                 </div>
             </div>
             <div className="comment-body">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+                <p>{ comment.text }</p>
             </div>
         </div>
       
